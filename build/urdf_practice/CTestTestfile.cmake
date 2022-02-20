55e54462dda83c10a791cf688b7b6fe5c55a2008
Vishnu Kumar <vishnu.kumardgp2002@gmail.com>
2022-02-20 16:36:59 +0530

    deleted all previous packages of ubuntu 16.04

--- build/urdf_practice/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vishnu/catkin_ws/src/urdf_practice
-# Build directory: /home/vishnu/catkin_ws/build/urdf_practice
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
